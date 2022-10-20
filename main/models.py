f8ae0dc20da58b9ccb64ca9a1a675b4a9ea7023c
Vitrola1302 <112354500+Vitrola1302@users.noreply.github.com>
2022-10-20 17:46:32 -0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Animal(models.Model):
+  nome_animal = models.CharField(max_length=256)
+  data_nascimento = models.DateField(null=True)
+  ativo = models.BooleanField(default=True)
