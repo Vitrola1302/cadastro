0407b817d6e13d924ef1772b9cfbd68fb4b502a8
Vitrola1302 <112354500+Vitrola1302@users.noreply.github.com>
2022-10-20 17:51:05 -0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.views.generic import TemplateView
+from .models import Animal
 
 class HomeView(TemplateView):
     template_name = 'main/index.html'
     
+class ListaAnimalView(ListView):
+    model = Animal
+    queryset = Animal.objects.all().order_by('nome_completo')
